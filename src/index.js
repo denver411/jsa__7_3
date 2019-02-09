3be7ded632f5bd2f1dadc13013865b2d180cd747
Denis Zhebov <denver411@yandex-team.ru>
2019-02-09 18:57:05 +0300

    Выполнена Задача №3 к лекции «Классы, наследование»

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import Bowman from './js/Bowman/Bowman';
+
+const bowman = new Bowman('1');
+const bowman1 = new Bowman('2');
+const bowman2 = new Bowman('3');
+
+// bowman.health = 5;
+bowman1.powerMode = 1;
+bowman1.attackEvent();
+bowman1.attackEvent();
+bowman1.attackEvent();
+bowman1.powerMode = 1;
+
+bowman2.levelUp();
+bowman2.attackEvent();
+bowman2.powerMode = 1;
+
+console.log(bowman, bowman.attack);
+console.log(bowman1, bowman1.attack);
+console.log(bowman2, bowman2.attack);
